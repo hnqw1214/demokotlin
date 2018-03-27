5a3142af9b49aba7d18c6022a16ddb7f612a0b77
hnqw1214 <hnqw1214@hnqw1214deMBP.lan>
2018-03-27 15:52:33 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/con/DataController.kt
@@ -0,0 +1,12 @@
+package com.example.demo.con
+
+import org.springframework.web.bind.annotation.*
+
+@RestController
+class DataController {
+
+    @GetMapping("/hello")
+    fun dataController(): String {
+        return "hello world"
+    }
+}
